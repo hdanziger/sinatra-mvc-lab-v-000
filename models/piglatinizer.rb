56b030005ff978a72deb7c1c838da2c616271ce9
Hannah Danziger <hdanziger8@gmail.com>
2017-11-13 15:07:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,7 @@ class PigLatinizer
     non_piglatin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
     vowels = ["a", "e", "i", "o", "u"]
 
+
     if non_piglatin_words.include?(word)
       word << "way"
     elsif vowels.include?(word[0])
