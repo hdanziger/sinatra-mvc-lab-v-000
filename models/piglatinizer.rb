fa25de233dcac80ac93a622f3ddb730001b3ec19
Hannah Danziger <hdanziger8@gmail.com>
2017-11-13 15:50:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinizer
 
   def piglatinize(word)
 
-    non_piglatin_words = ["I", "eighty", "alone", "Once", "old", "i", "me", "to", "too", "a", "an", "in", "and", "on", "until", "Enumeration"]
+    non_piglatin_words = ["I", "eighty", "alone", "Once", "upon", "old", "i", "me", "to", "too", "a", "an", "in", "and", "on", "until", "Enumeration"]
     vowels = ["a", "e", "i", "o", "u"]
 
     if non_piglatin_words.include?(word)
