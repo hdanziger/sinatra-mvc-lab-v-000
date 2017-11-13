f5d580365f73ded61b0458a8e220fc7f49e4878e
Hannah Danziger <hdanziger8@gmail.com>
2017-11-13 15:29:01 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinizer
 
   def piglatinize(word)
 
-    non_piglatin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    non_piglatin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on", "until"]
     vowels = ["a", "e", "i", "o", "u"]
 
     if non_piglatin_words.include?(word)
