79ee98cc1b918ce792819279607a8023331cfa5c
Hannah Danziger <hdanziger8@gmail.com>
2017-11-13 15:34:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    pl = PigLatinizer.new
+    p = PigLatinizer.new
     @piglatin = pl.to_pig_latin(params[:user_phrase])
     erb :results
   end
